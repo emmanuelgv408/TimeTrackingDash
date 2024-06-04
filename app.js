5426656d5410d1f51db912a39a6af6d344abc5b9
Emmanuel Guerrero Vega <emmanuelgv@live.com>
2024-06-04 14:49:02 -0700

    added functinality

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    const panels = document.querySelectorAll('.panels');
     const dailyButton = document.querySelector('.daily-button');
     const weeklyButton = document.querySelector('.weekly-button');
     const monthlyButton = document.querySelector('.monthly-button');
+    let currentHours = document.querySelectorAll('.current-hours');
+    let lastWeekHours = document.querySelectorAll('.last-week-hours');
 
+    let data;
 
-    async function fetchData() {
-
+    const fetchData = async () => {
         try {
             const res = await fetch('data.json');
-            const data = await res.json();
+            data = await res.json(); 
+            console.log(data);
 
-            return data;
+          
+            dailyButton.addEventListener('click', (e) => {
+                e.preventDefault();
+                updateUI('daily');
+            });
 
+            weeklyButton.addEventListener('click', (e) => {
+                e.preventDefault();
+                updateUI('weekly');
+            });
+
+            monthlyButton.addEventListener('click', (e) => {
+                e.preventDefault();
+                updateUI('monthly');
+            });
+
+        } catch (e) {
+            console.log('Oops, error at:', e);
         }
-        catch (e) {
-        console.log('Oops error at: ' e);
+    };
+
+    const updateUI = (period) => {
+        if (data) {
+            data.forEach((activity, index) => {
+                currentHours[index].innerHTML = `${activity.timeframes[period].current}hrs`;
+                lastWeekHours[index].innerHTML = `${activity.timeframes[period].previous}hrs`;
+            });
+        } else {
+            console.log(`Data for ${period} period is not available.`);
         }
+    };
+    
 
-    }
-
-
-
-})
-
+    fetchData(); 
+});
